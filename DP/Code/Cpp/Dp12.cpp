fd5ca8b2334a93e8f094674b1d0d95dc97815df1
PrathamD16 <pratham.doke.1602@gmail.com>
2023-05-01 15:36:20 +0530

    MatrixSumPath

--- DP/Code/Cpp/Dp12.cpp
+++ DP/Code/Cpp/Dp12.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 //Recursion
 int solve1(int i, int j, vector<vector<int>>&grid){
-    if(j < 0 || j > 0)return -1e9;
+    if(j < 0 || j > grid[0].size())return -1e9;
     if(i == 0)return grid[0][j];
     int s = grid[i][j] + solve1(i-1,j,grid);
     int ld = grid[i][j] + solve1(i-1,j-1,grid);
@@ -15,18 +15,63 @@ int solve1(int i, int j, vector<vector<int>>&grid){
     return max(s,max(ld,rd));
 }
 
+//Memoization
+int solve2(int i, int j, vector<vector<int>>&grid, vector<vector<int>>&dp){
+    if(j < 0 || j > grid[0].size())return -1e9;
+    if(i == 0)return grid[0][j];
+    if(dp[i][j] != -1)return dp[i][j];
+    int s = grid[i][j] + solve2(i-1,j,grid, dp);
+    int ld = grid[i][j] + solve2(i-1,j-1,grid, dp);
+    int rd = grid[i][j] + solve2(i-1,j+1,grid, dp);
+    return dp[i][j] = max(s,max(ld,rd));
+}
+
+//Tabulation form
+void solve3(vector<vector<int>>&grid, vector<vector<int>>&dp){
+    //Tabulation Form;
+    int row = grid.size();
+    int col = grid[0].size();
+    for(int j = 0; j < col; j++){
+        dp[0][j] = grid[0][j];
+    }
+
+    for(int i = 1; i < row; i++){
+        for(int j = 0; j < col; j++){
+            int s = grid[i][j];
+            int ld = grid[i][j];
+            int rd = grid[i][j];
+            s += dp[i-1][j];
+            if(j-1 >= 0){
+                ld += dp[i-1][j-1];
+            }
+            else ld += -1e8;
+            if(j+1 < col){
+                rd += dp[i-1][j+1];
+            }
+            else rd += -1e8;
+
+            dp[i][j] = max(s,max(ld,rd));
+        }   
+    }
+}
+
 int maxSumPath(vector<vector<int>>&grid){
     int row = grid.size();
     int cost = -1e9;
     int col = grid[0].size();
+    vector<vector<int>>dp(row, vector<int>(col, 0));
 
-    //As we can start from any column, so trying out all possible path
-    for(int j = 0; j < col; j++){
-        int pathCost = solve1(row-1,j,grid);
-        if(pathCost > cost){
-            cost = pathCost;
-        }
+    //Recursion
+    // for(int j = 0; j < col; j++){   
+    //     cost = max(cost, solve1(row-1, j, grid));
+    // }
+
+    //Tabulation
+    solve3(grid, dp);
+    for(int i = 0; i < col; i++){
+        cost = max(cost, dp[row-1][i]);
     }
+
     return cost;
 }
 
@@ -34,6 +79,6 @@ int main()
 {
     vector<vector<int>> grid = {{1, 2, 10, 4}, {100, 3, 2, 1}, {1, 1, 20, 2}, {1, 2, 2, 1}};
     cout << maxSumPath(grid);
-    cout << "Hello";
+    
     return 0;
 }
