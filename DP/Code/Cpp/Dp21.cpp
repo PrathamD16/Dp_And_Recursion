b3f631bfd9acacec6c84d89e2aebee8ee8864ef0
PrathamD16 <pratham.doke.1602@gmail.com>
2023-07-01 22:50:01 +0530

    Add Sign to array and match the target

---
+++ DP/Code/Cpp/Dp21.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//given a array 'nums' and a 'target'. return number of ways in which you can assign signs to number such that all the numbers in 'nums' summation is equal to 'target'
+
+
+//PRoblem is similar to dividing set into 2 subset whose difference is the target given;
+
+//Recursion
+
+//refer Dp18;
+
+
+int main(){
+    vector<int>nums = {1, 2, 3, 1};
+    int target = 3;
+    int n = nums.size();
+    
+    return 0;
+}
